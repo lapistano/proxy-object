38a9aa75d039f117d08ccb8b14015b7ae6fa573d
Bastian Feder <github@bastian-feder.de>
2012-10-20 17:16:07 +0200

    Use the autoloader for in testing env, added vendor dir to ignore list of git.

--- bootstrap.php
+++ bootstrap.php
@@ -2,10 +2,14 @@
 
 namespace lapistano\ProxyObject {
 
-    require_once __DIR__.'/src/lapistano/ProxyObject/ProxyBuilder.php';
-    require_once __DIR__.'/src/lapistano/ProxyObject/Generator.php';
-    require_once __DIR__.'/src/lapistano/ProxyObject/GeneratorException.php';
-
+    // unfortunately the autoloader is not just require a file.
     require_once __DIR__.'/Tests/Fixtures/Dummy.php';
+
+    // for the rest use the autoloader.
+    // Use
+    //   $loader->add($ns, $dir) to make a NS match to a dir not meeting PSR-0
+    //   $loader->addClassMap($dirList)  to add a specific file or dir to the autolaoder.
+
+    $loader = require_once __DIR__ . "/vendor/autoload.php";
 
 }
